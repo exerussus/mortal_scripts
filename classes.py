4d4c3563a7da8390e3aa6b8a5043c7513fa8c3da
exerussus <solovyov.production@gmail.com>
2022-04-28 17:23:37 +0300

    Optimization code, added Training and TimeActions classes, mental_training function converted to mental_training method.

---
+++ classes.py
@@ -0,0 +1,45 @@
+
+
+class TimeActions:
+
+    @staticmethod
+    def countdown(seconds):
+        from time import sleep
+        for i in range(seconds, 0, -1):
+            print(str(f'До старта {i} сек...'))
+            sleep(1)
+        print('Работает')
+
+class Training:
+
+    @staticmethod
+    def mental_training(repitition=10):
+        """Special mental training with water.
+        You need to set "spurt" into 1 cell and
+        "lesser heal" into 4 cell before using this
+        script. """
+        from pyautogui import press
+        from time import sleep
+        from random import uniform as between
+        from classes import TimeActions
+
+
+        TimeActions.countdown(3)
+
+        for l in range(repitition):
+
+            for i in range(50):
+                press('1')
+                sleep(between(1.7, 2.2))
+                press('q')
+                sleep(between(1.0, 1.5))
+
+            for i in range(5):
+                press('4')
+                sleep(between(1.7, 2.2))
+                press('q')
+
+            press('0')
+            sleep(between(24.0, 35.5))
+
+Training.mental_training()
--- test.py
+++ test.py
@@ -1,51 +1,53 @@
-import pyautogui
-import time
-import keyboard
-import threading
-
-
+# import pyautogui
+# import time
+# import keyboard
+# import threading
 #
-# # def trigger():
-# #     pyautogui.press('q')
+#
+# #
+# # # def trigger():
+# # #     pyautogui.press('q')
+# # #
+# # #
+# # # keyboard.add_hotkey('ctrl+shift', trigger)
+# #
+# # data_alt_key = {'key': ''}
 # #
 # #
-# # keyboard.add_hotkey('ctrl+shift', trigger)
+# # def press_block():
+# #     pass
+# #
+# #
+# # def key_watcher():
+# #     while keyboard.add_hotkey()
+# #     keyboard.add_hotkey('alt', press_block())
+# #     keyboard.wait('esc')
+# #
+# #
+# # thread_1 = threading.Thread(target=key_watcher)
+# # thread_2 = threading.Thread(target=alt_watcher)
+# # thread_1.start()
+# # thread_2.start()
 #
-# data_alt_key = {'key': ''}
+#
+# def blocking():
+#     keyboard.press('n')
 #
 #
-# def press_block():
-#     pass
+# def pressing_key():
+#     while True:
+#         holding_key = keyboard.is_pressed('alt')
+#         if holding_key:
+#             blocking()
 #
-#
-# def key_watcher():
-#     while keyboard.add_hotkey()
-#     keyboard.add_hotkey('alt', press_block())
-#     keyboard.wait('esc')
-#
-#
-# thread_1 = threading.Thread(target=key_watcher)
-# thread_2 = threading.Thread(target=alt_watcher)
-# thread_1.start()
-# thread_2.start()
+# # def key_watcher():
+# #     keyboard.add_hotkey('alt', blocking(pressing_key()))
+# #     keyboard.wait('esc')
+# #
+# #
+# # thread_watcher = threading.Thread(target=key_watcher)
+# thread_pressing_key = threading.Thread(target=pressing_key)
+# # thread_watcher.start()
+# thread_pressing_key.start()
 
 
-def blocking():
-    keyboard.press('n')
-
-
-def pressing_key():
-    while True:
-        holding_key = keyboard.is_pressed('alt')
-        if holding_key:
-            blocking()
-
-# def key_watcher():
-#     keyboard.add_hotkey('alt', blocking(pressing_key()))
-#     keyboard.wait('esc')
-#
-#
-# thread_watcher = threading.Thread(target=key_watcher)
-thread_pressing_key = threading.Thread(target=pressing_key)
-# thread_watcher.start()
-thread_pressing_key.start()
