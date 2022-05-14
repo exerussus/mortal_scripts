9fcee295edf6aa107d2c222e332e7292a4e58251
exerussus <solovyov.production@gmail.com>
2022-05-14 19:45:58 +0300

    Added Waiting class.

--- classes.py
+++ classes.py
@@ -143,6 +143,24 @@ class ResurrectingSuicide:
             sleep(between(2, 4))
 
 
+class Waiting:
+
+    @classmethod
+    def do(cls):
+
+        from keyboard import is_pressed
+        from time import sleep
+        from random import randrange as between
+        check_count = between(14, 18)
+        waiting_count = -1
+        while not is_pressed('ctrl'):
+            waiting_count += 1
+            print(f'Waiting: {check_count - waiting_count}')
+            if waiting_count == check_count:
+                break
+            sleep(1)
+
+
 class AdvancedMenu:
 
     @classmethod
